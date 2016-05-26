12b73a22dca8e72fbc0398e72c4135d6f178aaf3
jingweiwu <jingwei.wu@me.com>
2016-05-26 16:55:24 +0800

    Update "update maintenance task detail info" method & maintenance task class; Add code comments

--- WisdriIS/Connect/WISDataService/DataServiceCommon/WISMaintenanceTask.h
+++ WisdriIS/Connect/WISDataService/DataServiceCommon/WISMaintenanceTask.h
@@ -39,22 +39,43 @@ typedef NS_ENUM(NSInteger, MaintenanceTaskType) {
 
 @class WISMaintenanceTaskState;
 @interface WISMaintenanceTask : NSObject <NSCopying, NSCoding>
-
-///
+/// 维保任务 ID
 @property (readwrite, strong) NSString *taskID;
+/// 维保任务名称
 @property (readwrite, strong) NSString *taskName;
+/// 维保任务状态
 @property (readwrite, strong) NSString *state;
+/// 维保任务 - 是否已归档
+@property (readwrite, getter=isArchived, setter=archived:) BOOL archived;
+/// 维保任务流转过程的历史状态列表
 @property (readwrite, strong) NSMutableArray<WISMaintenanceTaskState *> *passedStates;
+/// 维保任务类型, 在WISMaintenanceTask.h中定义
 @property (readwrite) MaintenanceTaskType taskType;
+/// 维保任务所在工艺段名称
 @property (readwrite, strong) NSString *processSegmentName;
+/// 维保任务创建人
 @property (readwrite) WISUser *creator;
+/// 维保任务责任人
 @property (readwrite) WISUser *personInCharge;
+/// 维保任务详情描述
 @property (readwrite, strong) NSString *taskApplicationContent;
-@property (readwrite, strong) NSString *taskDescription;
+/// 维保任务备注说明 - 由前方部长在备注功能中填写
+@property (readwrite, strong) NSString *taskComment;
+/// 维保任务完成备注 - 由维保人员在维保任务完成时填写
+@property (readwrite, strong) NSString *taskFinishedRemark;
+/// 维保任务争议原因说明 - 由技术主管在发起争议时填写
+@property (readwrite, strong) NSString *disputeProcedureRemark;
+/// 维保任务归档说明 - 由技术主管在归档时填写
+@property (readwrite, strong) NSString *archivingRemark;
+/// 维保任务创建时间
 @property (readwrite, strong) NSDate *createdDateTime;
+/// 维保任务维保方案清单
 @property (readwrite, strong) NSMutableArray<WISMaintenancePlan *> *maintenancePlans;
+/// 维保任务评价
 @property (readwrite, strong) WISMaintenanceTaskRating *taskRating;
+/// 维保任务当前允许的操作列表
 @property (readwrite, strong) NSDictionary<NSString *, NSString *> *validOperations;
+/// 维保任务图片信息列表 (用于维保任务发起－生产人员添加图片, 以及维保任务结束-维保人员添加图片)
 @property (readwrite, strong) NSMutableDictionary<NSString *, WISFileInfo *> *imagesInfo;
 
 - (instancetype)init; // __attribute__((unavailable("init method not available")));
@@ -62,15 +83,19 @@ typedef NS_ENUM(NSInteger, MaintenanceTaskType) {
 - (instancetype)initWithTaskID:(NSString *) taskID
                       taskName:(NSString *) taskName
                      taskState:(NSString *) state
-              taskPassedStates:(NSMutableArray<WISMaintenanceTaskState *> *)passedStates
+                    isArchived:(BOOL) archieved
+              taskPassedStates:(NSMutableArray<WISMaintenanceTaskState *> *) passedStates
                       taskType:(MaintenanceTaskType) taskType
             processSegmentName:(NSString *) processSegmentName
                        creator:(WISUser *) creator
                 personInCharge:(WISUser *) personInCharge
         taskApplicationContent:(NSString *) taskApplicationContent
-               taskDescription:(NSString *) description
+                   taskComment:(NSString *) comment
+            taskFinishedRemark:(NSString *) finishedRemark
+        disputeProcedureRemark:(NSString *) disputeProcedureRemark
+               archivingRemark:(NSString *) archivingRemark
                createdDateTime:(NSDate *) createdDateTime
-               maintenancePlans:(NSMutableArray<WISMaintenancePlan *> *) maintenancePlans
+              maintenancePlans:(NSMutableArray<WISMaintenancePlan *> *) maintenancePlans
                     taskRating:(WISMaintenanceTaskRating *) taskRating
                validOperations:(NSDictionary<NSString *, NSString *> *) validOperations
                  andImagesInfo:(NSMutableDictionary<NSString *, WISFileInfo *> *) imagesInfo;
--- WisdriIS/Connect/WISDataService/DataServiceCommon/WISMaintenanceTask.m
+++ WisdriIS/Connect/WISDataService/DataServiceCommon/WISMaintenanceTask.m
@@ -14,13 +14,17 @@
 NSString *const maintenanceTaskIDEncodingID = @"maintenanceTaskID";
 NSString *const maintenanceTaskNameEncodingID = @"maintenanceTaskName";
 NSString *const maintenanceTaskStateEncodingID = @"maintenanceTaskState";
+NSString *const maintenanceTaskArchivedEncodingID = @"maintenanceTaskArchived";
 NSString *const maintenanceTaskPassedStatesEncodingID = @"maintenanceTaskPassedStates";
 NSString *const maintenanceTaskTypeEncodingID = @"maintenanceTaskType";
 NSString *const maintenanceTaskProcessSegmentNameEncodingID = @"maintenanceTaskProcessSegmentName";
 NSString *const maintenanceTaskCreatorEncodingID = @"maintenanceTaskCreator";
 NSString *const maintenanceTaskPersonInChargeEncodingID = @"maintenanceTaskPersonInCharge";
 NSString *const maintenanceTaskApplicationContentEncodingID = @"maintenanceTaskApplicationContent";
-NSString *const maintenanceTaskDescriptionEncodingID = @"maintenanceTaskDescription";
+NSString *const maintenanceTaskCommentEncodingID = @"maintenanceTaskComment";
+NSString *const maintenanceTaskFinishedRemarkEncodingID = @"maintenanceTaskFinishedRemark";
+NSString *const maintenanceTaskDisputeProcedureRemarkEncodingID = @"maintenanceTaskDisputeProcedureRemark";
+NSString *const maintenanceTaskArchivingRemarkEncodingID = @"maintenanceTaskArchivingRemark";
 NSString *const maintenanceTaskCreatedDateTimeEncodingID = @"maintenanceTaskCreatedDateTime";
 NSString *const maintenancePlansEncodingID = @"maintenancePlans";
 NSString *const maintenanceTaskRatingEncodingID = @"maintenanceTaskRating";
@@ -38,15 +42,19 @@ NSString *const maintenanceTaskImagesInfoEncodingID = @"maintenanceTaskImagesInf
     return [self initWithTaskID:@""
                        taskName:@""
                       taskState:@""
+                     isArchived:false
                taskPassedStates:[NSMutableArray array]
                        taskType:MaintenanceTaskUnclassified
              processSegmentName:@""
                         creator:[[WISUser alloc]init]
                  personInCharge:[[WISUser alloc]init]
          taskApplicationContent:@""
-                taskDescription:@""
+                    taskComment:@""
+             taskFinishedRemark:@""
+         disputeProcedureRemark:@""
+                archivingRemark:@""
                 createdDateTime:[NSDate date]
-                maintenancePlans:[NSMutableArray array]
+               maintenancePlans:[NSMutableArray array]
                      taskRating:[[WISMaintenanceTaskRating alloc]init]
                 validOperations:[NSDictionary dictionary]
                   andImagesInfo:[NSMutableDictionary dictionary]];
@@ -56,13 +64,17 @@ NSString *const maintenanceTaskImagesInfoEncodingID = @"maintenanceTaskImagesInf
 - (instancetype)initWithTaskID:(NSString *) taskID
                       taskName:(NSString *) taskName
                      taskState:(NSString *) state
-              taskPassedStates:(NSMutableArray<WISMaintenanceTaskState *> *)passedStates
+                    isArchived:(BOOL) archieved
+              taskPassedStates:(NSMutableArray<WISMaintenanceTaskState *> *) passedStates
                       taskType:(MaintenanceTaskType) taskType
             processSegmentName:(NSString *) processSegmentName
                        creator:(WISUser *) creator
                 personInCharge:(WISUser *) personInCharge
         taskApplicationContent:(NSString *) taskApplicationContent
-               taskDescription:(NSString *) description
+                   taskComment:(NSString *) comment
+            taskFinishedRemark:(NSString *) finishedRemark
+        disputeProcedureRemark:(NSString *) disputeProcedureRemark
+               archivingRemark:(NSString *) archivingRemark
                createdDateTime:(NSDate *) createdDateTime
               maintenancePlans:(NSMutableArray<WISMaintenancePlan *> *) maintenancePlans
                     taskRating:(WISMaintenanceTaskRating *) taskRating
@@ -72,13 +84,17 @@ NSString *const maintenanceTaskImagesInfoEncodingID = @"maintenanceTaskImagesInf
         _taskID = taskID;
         _taskName = taskName;
         _state = state;
+        _archived = archieved;
         _passedStates = passedStates;
         _taskType = taskType;
         _processSegmentName = processSegmentName;
         _creator = creator;
         _personInCharge = personInCharge;
         _taskApplicationContent = taskApplicationContent;
-        _taskDescription = description;
+        _taskComment = comment;
+        _taskFinishedRemark = finishedRemark;
+        _disputeProcedureRemark = disputeProcedureRemark;
+        _archivingRemark = archivingRemark;
         _createdDateTime = createdDateTime;
         _maintenancePlans = maintenancePlans;
         _taskRating = taskRating;
@@ -94,13 +110,17 @@ NSString *const maintenanceTaskImagesInfoEncodingID = @"maintenanceTaskImagesInf
         _taskID = (NSString *)[aDecoder decodeObjectForKey:maintenanceTaskIDEncodingID];
         _taskName = (NSString *)[aDecoder decodeObjectForKey:maintenanceTaskNameEncodingID];
         _state = (NSString *)[aDecoder decodeObjectForKey:maintenanceTaskStateEncodingID];
+        _archived = (BOOL)[aDecoder decodeBoolForKey:maintenanceTaskArchivedEncodingID];
         _passedStates = [NSMutableArray arrayWithArray:(NSArray *)[aDecoder decodeObjectForKey:maintenanceTaskPassedStatesEncodingID]];
         _taskType = (MaintenanceTaskType)[aDecoder decodeIntegerForKey:maintenanceTaskTypeEncodingID];
         _processSegmentName = (NSString *)[aDecoder decodeObjectForKey:maintenanceTaskProcessSegmentNameEncodingID];
         _creator = (WISUser *)[aDecoder decodeObjectForKey:maintenanceTaskCreatorEncodingID];
         _personInCharge = (WISUser *)[aDecoder decodeObjectForKey:maintenanceTaskPersonInChargeEncodingID];
         _taskApplicationContent = (NSString *)[aDecoder decodeObjectForKey:maintenanceTaskApplicationContentEncodingID];
-        _taskDescription = (NSString *)[aDecoder decodeObjectForKey:maintenanceTaskDescriptionEncodingID];
+        _taskComment = (NSString *)[aDecoder decodeObjectForKey:maintenanceTaskCommentEncodingID];
+        _taskFinishedRemark = (NSString *)[aDecoder decodeObjectForKey:maintenanceTaskFinishedRemarkEncodingID];
+        _disputeProcedureRemark = (NSString *)[aDecoder decodeObjectForKey:maintenanceTaskDisputeProcedureRemarkEncodingID];
+        _archivingRemark = (NSString *)[aDecoder decodeObjectForKey:maintenanceTaskArchivingRemarkEncodingID];
         _createdDateTime = (NSDate *)[aDecoder decodeObjectForKey:maintenanceTaskCreatedDateTimeEncodingID];
         _maintenancePlans = [NSMutableArray arrayWithArray:(NSArray *)[aDecoder decodeObjectForKey:maintenancePlansEncodingID]];
         _taskRating = (WISMaintenanceTaskRating *)[aDecoder decodeObjectForKey:maintenanceTaskRatingEncodingID];
@@ -114,13 +134,17 @@ NSString *const maintenanceTaskImagesInfoEncodingID = @"maintenanceTaskImagesInf
     [aCoder encodeObject:_taskID forKey:maintenanceTaskIDEncodingID];
     [aCoder encodeObject:_taskName forKey:maintenanceTaskNameEncodingID];
     [aCoder encodeObject:_state forKey:maintenanceTaskStateEncodingID];
+    [aCoder encodeBool:_archived forKey:maintenanceTaskArchivedEncodingID];
     [aCoder encodeObject:_passedStates forKey:maintenanceTaskPassedStatesEncodingID];
     [aCoder encodeInteger:_taskType forKey:maintenanceTaskTypeEncodingID];
     [aCoder encodeObject:_processSegmentName forKey:maintenanceTaskProcessSegmentNameEncodingID];
     [aCoder encodeObject:_creator forKey:maintenanceTaskCreatorEncodingID];
     [aCoder encodeObject:_personInCharge forKey:maintenanceTaskPersonInChargeEncodingID];
     [aCoder encodeObject:_taskApplicationContent forKey:maintenanceTaskApplicationContentEncodingID];
-    [aCoder encodeObject:_taskDescription forKey:maintenanceTaskDescriptionEncodingID];
+    [aCoder encodeObject:_taskComment forKey:maintenanceTaskCommentEncodingID];
+    [aCoder encodeObject:_taskFinishedRemark forKey:maintenanceTaskFinishedRemarkEncodingID];
+    [aCoder encodeObject:_disputeProcedureRemark forKey:maintenanceTaskDisputeProcedureRemarkEncodingID];
+    [aCoder encodeObject:_archivingRemark forKey:maintenanceTaskArchivingRemarkEncodingID];
     [aCoder encodeObject:_createdDateTime forKey:maintenanceTaskCreatedDateTimeEncodingID];
     [aCoder encodeObject:_maintenancePlans forKey:maintenancePlansEncodingID];
     [aCoder encodeObject:_taskRating forKey:maintenanceTaskRatingEncodingID];
@@ -133,13 +157,17 @@ NSString *const maintenanceTaskImagesInfoEncodingID = @"maintenanceTaskImagesInf
     WISMaintenanceTask *task = [[[self class] allocWithZone:zone] initWithTaskID:[self.taskID copy]
                                                                         taskName:[self.taskName copy]
                                                                        taskState:[self.state copy]
+                                                                      isArchived:self.isArchived
                                                                 taskPassedStates:[self.passedStates mutableCopy]
                                                                         taskType:_taskType
                                                               processSegmentName:[self.processSegmentName copy]
                                                                          creator:[self.creator copy]
                                                                   personInCharge:[self.personInCharge copy]
                                                           taskApplicationContent:[self.taskApplicationContent copy]
-                                                                 taskDescription:[self.taskDescription copy]
+                                                                     taskComment:[self.taskComment copy]
+                                                              taskFinishedRemark:[self.taskFinishedRemark copy]
+                                                          disputeProcedureRemark:[self.disputeProcedureRemark copy]
+                                                                 archivingRemark:[self.archivingRemark copy]
                                                                  createdDateTime:[self.createdDateTime copy]
                                                                  maintenancePlans:[self.maintenancePlans mutableCopy]
                                                                       taskRating:[self.taskRating copy]
